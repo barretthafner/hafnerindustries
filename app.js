29f37577d6633f56a70f6c399e74e77c574bb05f
Barrett Hafner <barrett@hafnerindustries.com>
2016-05-05 14:24:47 -0700

    use gulp to build site

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express  = require("express");
 var app = express();
 
-app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.listen(process.env.PORT, process.env.IP, function () {
